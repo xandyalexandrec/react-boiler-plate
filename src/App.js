429d64142f2eb6e763dcbf6217ff34cfcf9d4479
Alexandre Custodio <xandy.alexandrec@gmail.com>
2019-02-03 19:25:07 -0200

    feat: add  router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => (
-  <>
-    <p>teste</p>
-  </>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </Router>
 );
 
 export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <p>home</p>
+  </>
+);
+
+export default Home;
